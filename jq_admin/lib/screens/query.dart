d90ae0a9292b66844e52b003879bb0ca56c2bd74
Jillian <jillian.tenerife@usjr.edu.ph>
2023-07-31 22:57:23 +0800

    openai key changes

--- jq_admin/lib/screens/query.dart
+++ jq_admin/lib/screens/query.dart
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage> {
       });
     }
 
-    final url = Uri.parse('http://192.168.68.110:7999/query');
+    final url = Uri.parse('http://192.168.68.124:7999/query');
     final headers = {'Content-Type': 'application/json'};
     final body = jsonEncode({
       'question': message,
