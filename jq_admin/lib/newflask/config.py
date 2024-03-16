b2abbea776feb0f3f041000f961e732b8e4c2e21
Garfield Lim <garfield.lim@usjr.edu.ph>
2024-03-16 17:31:38 +0800

    config

--- jq_admin/lib/newflask/config.py
+++ jq_admin/lib/newflask/config.py
@@ -5,23 +5,23 @@ from firebase_admin import credentials
 # config.py
 MILVUS_HOST = "localhost"
 MILVUS_PORT = "19530"
-OPENAI_API_KEY = "C:/Users/JillianDocumentsAPI.txt"
+OPENAI_API_KEY = "/Users/garfieldgreglim/Downloads/API.txt"
 TOKENIZER_PATH = "intfloat/e5-large-v2"
 
 SVM_MODEL = load(
-    "lib/newflask/models/svm_model.joblib"
+    "/Users/garfieldgreglim/Documents/JQ/jq_admin/lib/newflask/models/svm_model.joblib"
 )
 LABEL_ENCODER = load(
-    "lib/newflask/models/label_encoder.joblib"
+    "/Users/garfieldgreglim/Documents/JQ/jq_admin/lib/newflask/models/label_encoder.joblib"
 )
 
 CRED = credentials.Certificate(
-    "lib/newflask/utils/josenianquiri-c3c63-firebase-adminsdk-r8ews-1dd8ff0c6e.json"
+    "/Users/garfieldgreglim/Documents/JQ/jq_admin/lib/newflask/utils/josenianquiri-c3c63-firebase-adminsdk-r8ews-1dd8ff0c6e.json"
 )
 
 POSTS_JSON_PATH = "/Users/garfieldgreglim/Documents/JQ/jq_admin/web/assets/posts.json"
 COOKIES_PATH = (
-    "lib/newflask/utils/cookies.json"
+    "/Users/garfieldgreglim/Documents/JQ/jq_admin/lib/newflask/utils/cookies.json"
 )
 
-MAIN_POSTS_JSON_PATH = "C:/Users/Jillian/Documents/JQ/jq_admin/posts.json"
+MAIN_POSTS_JSON_PATH = "/Users/garfieldgreglim/Documents/JQ/jq_admin/posts.json"
